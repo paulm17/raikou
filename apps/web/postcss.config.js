282f305fc3bb8e3f95b37d999518cd14fef4eee0
Paul Moss <paulmoss06@gmail.com>
2023-07-20 13:51:25 +0100

    another commit

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -5,13 +5,13 @@ module.exports = {
     "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
-    "postcss-removecss-raikou": {
-      appPath: "./app",
-      libPath: [
-        "./node_modules/@raikou/client/src/index.ts",
-        "./node_modules/@raikou/server/src/index.ts",
-      ],
-      exts: [".tsx"],
-    },
+    // "postcss-removecss-raikou": {
+    //   appPath: "./app",
+    //   libPath: [
+    //     "./node_modules/@raikou/client/src/index.ts",
+    //     "./node_modules/@raikou/server/src/index.ts",
+    //   ],
+    //   exts: [".tsx"],
+    // },
   },
 };
