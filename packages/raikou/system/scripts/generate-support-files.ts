37eacbf0dc9daa30924151cc6e2f8522a05685c0
Paul Moss <paulmoss06@gmail.com>
2023-12-26 17:57:07 +0000

    build: another commit

--- packages/raikou/system/scripts/generate-support-files.ts
+++ packages/raikou/system/scripts/generate-support-files.ts
@@ -1,20 +1,7 @@
-const fs = require("fs");
 import { componentNames } from "./component-names";
 import { cssModules } from "./css-modules";
 
-function check() {
-  const workingDir = process.cwd();
-  const folderPath = `${workingDir}/dist`;
+process.chdir("packages/raikou/system");
 
-  // check whether system folder has dist folder
-  if (fs.existsSync(folderPath)) {
-    console.log("Building support files...");
-
-    componentNames();
-    cssModules();
-  } else {
-    console.log("System has no dist folder, skipping...");
-  }
-}
-
-check();
+componentNames();
+cssModules();
