0b4dd61eef423a888a1581a59f1b65d67e9cc690
Paul Moss <paulmoss06@gmail.com>
2024-01-01 11:54:10 +0000

    build: another commit

--- apps/docs/postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-preset-raikou": {},
-    "postcss-discard-duplicates": {},
-  },
-};
--- packages/raikou/components/Dummy/src/index.ts
+++ packages/raikou/components/Dummy/src/index.ts
@@ -1,5 +1,5 @@
 function Hello() {
-  console.log("hello - A0016-05");
+  console.log("hello - A0018-05");
 }
 
 export { Hello };
