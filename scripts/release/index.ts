658cd6184a5f83a8597a548b6d3c47fb5b5c57e4
Paul Moss <paulmoss06@gmail.com>
2024-11-11 13:53:57 +0000

    build: another commit

--- scripts/release/index.ts
+++ scripts/release/index.ts
@@ -31,8 +31,6 @@ const { argv }: { argv: any } = yargs(hideBin(process.argv))
 async function release() {
   const status = await git.status();
 
-  console.log(status);
-
   if (status.files.length !== 0) {
     logger.error('Working tree is not clean');
     process.exit(1);
